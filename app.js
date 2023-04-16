54e1af65a48b47d612a3da9daf3c8593dc3ac71e
Aaliyan Ahmed <121717147+AaliyanAhmed@users.noreply.github.com>
2023-04-16 23:40:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+(async ()=>{
+    const response = await fetch("./data.json");
+    const movies = await response.json();
+    console.log(movies);
+
+    const genreElem = document.getElementById("searchGenre");
+    const yearElem = document.getElementById("searchYear");
+    const languageElem = document.getElementById("searchLanguage");
+    const ratingElem = document.getElementById("searchRating");
+    const table = document.getElementById("dataOutput");
+
+function displaySearchResult(results){
+    table.innerHTML =" ";
+    counter = 1;
+    results.forEach((movie)=>{
+        let date =movie.release_date.split("-");
+        let year = date[0];
+        table.innerHTML += `
+         <tr class="bg-white border-b dark:bg-gray-200 text-gray-900 dark:border-gray-700">
+             <td class="px-6 py-3">${counter}</td>
+             <td class="px-6 py-3">${movie.title}</td>
+             <td class="px-6 py-3">${year}</td>
+         </tr>
+      `;
+     counter++;
+    
+    })
+}
+
+
+
+function valCheck(value){
+    if(value == 'all'){return true}
+    return false;
+}
+
+
+
+
+function search(){
+    const genreQuery = genreElem.value.toLowerCase();
+    const yearQuery = yearElem.value.toLowerCase();
+    const languageQuery = languageElem.value.toLowerCase();
+    const ratingQuery = ratingElem.value.toLowerCase();
+
+
+    
+    const results = movies.filter(function(movie){
+        let date =movie.release_date.split("-");
+        let year = date[0];
+        console.log(yearQuery);
+        if((!valCheck(languageQuery)) && (!valCheck(ratingQuery) && (!valCheck(yearQuery) && (!valCheck(genreQuery))))){
+            return( 
+                movie.original_language.toLowerCase().includes(languageQuery) &&
+                movie.vote_average >= ratingQuery && movie.genres.toString(" ").toLowerCase().includes(genreQuery) && year == yearQuery ) 
+            }
+        else if ((valCheck(languageQuery)) && (!valCheck(ratingQuery) && (!valCheck(yearQuery) && (!valCheck(genreQuery))))){
+            return(
+                movie.vote_average >= ratingQuery && movie.genres.toString(" ").toLowerCase().includes(genreQuery) && year == yearQuery
+            )
+        }
+        else if ((!valCheck(languageQuery)) && (valCheck(ratingQuery) && (!valCheck(yearQuery) && (!valCheck(genreQuery))))){
+            return( 
+                movie.original_language.toLowerCase().includes(languageQuery) && movie.genres.toString(" ").toLowerCase().includes(genreQuery) 
+                && year == yearQuery ) 
+        }
+        else if ((!valCheck(languageQuery)) && (!valCheck(ratingQuery) && (valCheck(yearQuery) && (!valCheck(genreQuery))))){
+            return( 
+                movie.original_language.toLowerCase().includes(languageQuery) &&
+                movie.vote_average >= ratingQuery && movie.genres.toString(" ").toLowerCase().includes(genreQuery) 
+                ) 
+        }
+        else if((!valCheck(languageQuery)) && (!valCheck(ratingQuery) && (!valCheck(yearQuery) && (valCheck(genreQuery))))){
+            return( 
+                movie.original_language.toLowerCase().includes(languageQuery) &&
+                movie.vote_average >= ratingQuery && year == yearQuery ) 
+            }
+    })
+    console.log(results);
+    displaySearchResult(results);
+
+}
+btnElem.addEventListener("click", search);
+
+
+
+
+})
